b6ac7e82fb58a332225346421509e6ef861108c3
AzureAD\OuraniaPapadopoulou <papadopoulou@mik3.gr>
2026-05-05 09:42:31 +0300

    Init

--- BlazorTest/obj/Debug/net10.0/BlazorTest.AssemblyInfo.cs
+++ BlazorTest/obj/Debug/net10.0/BlazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11219544ae2aec80bc1661944030a9b9730c2fee")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
